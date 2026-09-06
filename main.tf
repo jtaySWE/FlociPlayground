c602b979529b691ca4374224d3f6b9bb7432b2ab
Jason Tay <jasontay2013@gmail.com>
2026-09-07 00:57:25 +1000

    added volume of docker.sock for floci aws. downgraded aws required provider version to 5

--- main.tf
+++ main.tf
@@ -1,9 +1,10 @@
 resource "aws_mq_broker" "flociMQ" {
   broker_name = "flociAwsMq"
 
-  engine_type        = "RabbitMQ"
+  engine_type        = "RABBITMQ"
   engine_version     = "5.17.6"
   host_instance_type = "mq.t2.micro"
+  tags               = {}
 
   user {
     username = "flociUser"
